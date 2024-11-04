24cadb144df951615bd53b5e01c4cae29b034632
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-11-03 16:29:56 -0800

    initialized D3 with button and message

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+import "./style.css";
+const map: HTMLDivElement = document.querySelector("#map");
+
+const button = document.createElement("button");
+button.innerHTML = "Click me";
+button.addEventListener("click", () => {
+  //I want it to say you clicked me on the screen
+  document.body.innerHTML = "You clicked me";
+});
+map.append(button);
