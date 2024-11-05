b6c1c4e96daff69d1ee3f26f0f43f78c600b2563
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-11-04 22:04:35 -0800

    added in geocaches with popups and dummy buttons

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import "./style.css";
 import "./leafletWorkaround.ts";
 
 // Derministic randome number generator
-//import luck from "./luck.ts";
+import luck from "./luck.ts";
 
 //create cell interface for mapping
 interface Cell {
@@ -23,9 +23,9 @@ const LOCATION = leaflet.latLng(playerCell.i, playerCell.j);
 
 //Gameplay map variables
 const ZOOM_LEVEL = 18;
-//const TILE_DEGREES = 1E-4;
-//const NEIGHBORHOOD_SIZE = 8;
-//const CACHE_SPAWN_PROBABILITY = 0.1;
+const TILE_DEGREES = 1E-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 
 //create a map
 const map = leaflet.map(document.getElementById("map")!, {
@@ -49,3 +49,72 @@ leaflet
 const playerMarker = leaflet.marker(LOCATION);
 playerMarker.bindTooltip("You are here");
 playerMarker.addTo(map);
+
+//Display  player points
+let playerPoints = 0;
+const statusPanel = document.querySelector("#statusPanel")!; // defined in index.html
+statusPanel.textContent = `Points: ${playerPoints}`;
+
+//interface of coins that holds a cell and points (for now)
+interface Coins {
+  readonly cell: Cell;
+  readonly coins: number;
+}
+
+// inface of cache that holds coins
+interface Cache {
+  readonly coins: Coins[];
+}
+
+//add caches tot he map with cells
+function spawnCache(newCell: Cell) {
+  const origin = LOCATION;
+  const offset = 1;
+  const bounds = leaflet.latLngBounds([
+    [
+      origin.lat + newCell.i * TILE_DEGREES,
+      origin.lng + newCell.j * TILE_DEGREES,
+    ],
+    [
+      origin.lat + (newCell.i + offset) * TILE_DEGREES,
+      origin.lng + (newCell.j + offset) * TILE_DEGREES,
+    ],
+  ]);
+
+  //adds rectangle to map
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  rect.bindPopup(() => {
+    //Each Cache has a random point value, mutable by the player
+    let pointValue = Math.floor(
+      luck([newCell.i, newCell.j, "initialValue"].toString()) * 100,
+    );
+
+    //popup description and button
+    const popUp = document.createElement("div");
+    popUp.innerHTML =
+      `<div>You found a cache! Location: ${newCell.i}, ${newCell.j}.  Points: ${pointValue}</div>
+        <button id="collectButton">Collect</button>`;
+
+    //Clicking button decrements cache value and increments player points
+    popUp.querySelector<HTMLButtonElement>("#collectButton")!
+      .addEventListener("click", () => {
+        pointValue--;
+        popUp.querySelector<HTMLSpanElement>("#pointValue")!.innerHTML =
+          `Points: ${pointValue}`;
+        playerPoints++;
+        statusPanel.innerHTML = `Points: ${playerPoints}`;
+      });
+    return popUp;
+  });
+}
+
+// check player location and find caches 10% of the time
+for (let i = -NEIGHBORHOOD_SIZE; i <= NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j <= NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache({ i, j });
+    }
+  }
+}
