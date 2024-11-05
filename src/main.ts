289322735fafbbdbd4ea65dcce1dce9fcb8665e6
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-11-04 16:30:17 -0800

    added in map with tile layer

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,46 @@
-import "./style.css";
-const map: HTMLDivElement = document.querySelector("#map")!;
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
 
-//button placement for workability
-const button = document.createElement("button");
-button.innerText = "Click me";
-button.id = "button";
-button.addEventListener("click", () => {
-  button.innerText = "You clicked me";
+//style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Derministic randome number generator
+//import luck from "./luck.ts";
+
+//create cell interface for mapping
+interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
+
+//location set to Oakes Classroom
+const LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+//Gameplay map variables
+const ZOOM_LEVEL = 18;
+//const TILE_DEGREES = 1E-4;
+//const NEIGHBORHOOD_SIZE = 8;
+//const CACHE_SPAWN_PROBABILITY = 0.1;
+
+//create a map
+const map = leaflet.map(document.getElementById("map")!, {
+  center: LOCATION,
+  zoom: ZOOM_LEVEL,
+  minZoom: ZOOM_LEVEL,
+  maxZoom: ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
-map.append(button);
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
